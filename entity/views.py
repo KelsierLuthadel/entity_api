d4197655519b38c81fd1e1f94e2741a6b6cf0d4d
Kelsier Luthadel <kelsider.luthadel@gmail.com>
2022-09-02 18:22:01 +0100

    Added permissions

--- entity/views.py
+++ entity/views.py
@@ -8,17 +8,20 @@ from entity.serializers import EntitySerializer, AddressSerializer, ResourceSeri
 
 class ResourceViewSet(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ResourceSerializer
 
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = AddressSerializer
     filterset_class = AddressFilter
 
 
 class EntityViewSet(viewsets.ModelViewSet):
     queryset = Entity.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = EntitySerializer
     filterset_class = EntitiesFilter
 
--- entity_api/settings.py
+++ entity_api/settings.py
@@ -138,10 +138,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
